97d9c37c631193f81d12257570bed18f281ef508
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-05 22:36:18 -0600

    register user

--- routers/user.router.js
+++ routers/user.router.js
@@ -5,10 +5,10 @@ const UserMiddleware = require('../middlewares/user.middleware')
 
 
 router.post('/login', UserMiddleware.login, UserController.login)
+router.post('/register', UserMiddleware.create, UserController.create)
 
 // router.get('/users', UserMiddleware.get, UserController.get)
 router.get('/users/status', UserMiddleware.getStatus, UserController.getStatus)
-router.post('/users', UserMiddleware.create, UserController.create)
 router.put('/users', UserMiddleware.update, UserController.update)
 router.put('/users/status', UserMiddleware.updateStatus, UserController.updateStatus)
 router.delete('/users', UserMiddleware.delete, UserController.delete)
