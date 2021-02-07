c286b35c46aff86fc7d6651cb496775fc0e22c5e
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-07 17:55:01 -0600

    fix key indexation, and added controlNum optional but unique if value is string

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -4,11 +4,19 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
 	email: { type: String, unique: true },
 	username: { type: String, unique: true },
-	publicKey: { type: String, unique: true },
-	sessionId: { type: String, default: null },
+	publicKey: { type: String },
+	sessionId: { type: String, default: null, unique: true },
 	password: { type: String, select: false },
-	controlNum: { type: String, default: null, select: false },
-	privateKey: { type: String, unique: true, select: false },
+	controlNum: {
+		type: String,
+		index: {
+			unique: true,
+			partialFilterExpression: { controlNum: { $type: 'string' } },
+		},
+		default: null,
+		select: false,
+	},
+	privateKey: { type: String, select: false },
 	controlPwd: { type: String, default: null, select: false },
 	active: { type: Boolean, default: true, select: false },
 })
