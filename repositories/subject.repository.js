4aa50ec27ac335eaa1ccae9ccf3a714adf7b1730
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-07 23:44:44 -0600

    updated subject repo

--- repositories/subject.repository.js
+++ repositories/subject.repository.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const SubjectSchema = new Schema({
-	clave: { type: String, unique: true },
+	key: { type: String, unique: true },
 	name: { type: String },
 	career: { type: String, default: null },
 })
