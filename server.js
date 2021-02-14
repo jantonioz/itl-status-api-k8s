9d1703cce53a167a33081faaa1aadcc8aadb8375
antoniozandate <antonio.zandate@metricamovil.com>
2021-02-13 22:10:14 -0600

    using graphql

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const app = require('express')()
 const bodyParser = require('body-parser')
 const rateLimit = require('express-rate-limit')
+
 const db = require('./database')
 const config = require('./config').server
 
@@ -42,6 +43,8 @@ app.use(`${API_URL}/register`, registerLimit)
 app.use(`${API_URL}/login`, registerLimit, loginLimit)
 
 app.use(API_URL, require('./routers'))
+
+app.use('/graphql', require('./graphql'))
 
 async function startServer() {
 	try {
