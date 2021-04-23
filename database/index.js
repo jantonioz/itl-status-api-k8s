32a5b0d3bfee92c165263963130174e1d6f0658c
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-04-22 21:42:27 -0500

    changed authentication database

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const cred = require('../config').database
 
 async function connect() {
-	const str = `mongodb://${cred.username}:${cred.password}@${cred.host}:${cred.port}/${cred.name}?authSource=admin`
+	const str = `mongodb://${cred.username}:${cred.password}@${cred.host}:${cred.port}/${cred.name}?authSource=itlstatus`
 	return mongoose.connect(str, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
